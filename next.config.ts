8974f91bc587b6515feb49f8633cd501ee8373cf
Surya Veer Singh <suryaveer046@gmail.com>
2026-02-14 01:21:28 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from 'next';  
-const nextConfig: NextConfig = { 
+const nextConfig: NextConfig = {  
   /* config options here */ 
   typescript: {
     ignoreBuildErrors: true,
